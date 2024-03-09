8ebd01a1d2c1cfcd6fcedf66e6f9e71f69101e34
PratikTalaviya <pratiktalaviya33@gmail.com>
2024-03-09 10:01:26 +0530

    reupdate auth.js

--- taskhub-backend/routes/auth.js
+++ taskhub-backend/routes/auth.js
@@ -4,7 +4,7 @@ const authController = require("../controllers/authController");
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
-router.get("/google", authController.googleAuth);
-router.get("/google/callback", authController.googleAuthCallback, authController.googleAuthCallbackHandler);
+// router.get("/google", authController.googleAuth);
+// router.get("/google/callback", authController.googleAuthCallback, authController.googleAuthCallbackHandler);
 
 module.exports = router;
