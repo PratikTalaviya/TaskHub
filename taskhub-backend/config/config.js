5cc934e39ed6980cce3ba1f62690b412313b8f05
PratikTalaviya <pratiktalaviya33@gmail.com>
2024-03-09 11:14:24 +0530

    Updated config.js

--- taskhub-backend/config/config.js
+++ taskhub-backend/config/config.js
@@ -1,9 +1,4 @@
 module.exports = {
   secretKey: "679404fdd87a372a873aa636f9bba12b398e53785c1c6c25004315ffd33d3452",
   mongoURI: process.env.mongoURI,
-  // google: {
-  //   clientID: "1033418797319-hit014tr38kn6g8vputk4ls63cmkajas.apps.googleusercontent.com",
-  //   clientSecret: "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",
-  //   callbackURL: ""
-  // },
 };
