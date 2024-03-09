7540ffbe1aa220a6a664511759e5fc408ddc8845
PratikTalaviya <pratiktalaviya33@gmail.com>
2024-03-09 11:15:45 +0530

    Updated config.js

--- taskhub-backend/config/config.js
+++ taskhub-backend/config/config.js
@@ -1,8 +1,4 @@
 module.exports = {
   secretKey: process.env.JWT_SECRET || "679404fdd87a372a873aa636f9bba12b398e53785c1c6c25004315ffd33d3452",
   mongoURI: process.env.mongoURI,
-  google: {
-    clientID: "YOUR_CLIENT_ID",
-    clientSecret: "YOUR_CLIENT_SECRET",
-  },
 };
